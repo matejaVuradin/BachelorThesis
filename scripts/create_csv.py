e6be3efe6137fb647f59548003053bc00ccbc8b5
Mateja Vuradin <mv53070@fer.hr>
2023-05-29 13:00:13 +0200

    update scripts

--- scripts/create_csv.py
+++ scripts/create_csv.py
@@ -4,9 +4,8 @@ import math
 from os.path import exists
 
 tsvFile = sys.argv[1]
-#print(tsvFile);
-scanNum = int(sys.argv[2]); #neki ispitanici imaju vise scenova
-csvFile = sys.argv[3] + "datasetT1_300_c.csv"
+scanNum = int(sys.argv[2]) #neki ispitanici imaju vise scenova
+csvFile = sys.argv[3] + "datasetT1_100.csv"
 
 datasetCSV = []
 with open(tsvFile) as tFile:
@@ -22,7 +21,7 @@ scan_id = data[1]
 if scan_id == "1":
 	t1 = sys.argv[4]
 sub_str = "_ses-"
-t1_path = t1[:t1.index(sub_str) + len(sub_str)] + subject_id+ "_desc-restore_T1w.nii.gz"
+t1_path = t1[:t1.index(sub_str) + len(sub_str)] + subject_id+"_desc-restore_T1w.nii.gz"
 age_at_scan = data[3]
 age = math.floor(float(data[3]))
 datasetCSV.append([t1_path, subject_id, age_at_scan, age])
